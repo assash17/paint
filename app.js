277ce2615409d2f6e7f792b071f2e3b6fa1955a6
JeongSeonghoon <assash17@gmail.com>
2020-11-08 16:22:35 +0900

    first version

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const DEFAULT_STROKE_STYLE="black";
+const DEFAULT_STYLE="black";
 const DEFAULT_LINE_WIDTH=1;
 
 const canvas = document.getElementById("canvas");
@@ -6,8 +6,17 @@ const canvas = document.getElementById("canvas");
 // canvas.width = 500;
 // canvas.height = 500;
 const ctx = canvas.getContext("2d");
-const colors = document.querySelectorAll(".control__color");
 let painting = false;
+
+const modeRadio = document.querySelectorAll("input[name=mode]");
+let filling = false;
+
+const brushSize = document.getElementById("jsRange")
+
+const clearBtn = document.getElementById("jsClear");
+const saveBtn = document.getElementById("jsSave");
+
+const colors = document.querySelectorAll(".control__color");
 
 const setPainting = (isPaint) => {
     painting = isPaint;
@@ -19,11 +28,11 @@ const onMouseMove = (event) => {
     if(!painting){
         ctx.beginPath();
         ctx.moveTo(x,y);
-        console.log("create move",x,y);
+        // console.log("create move",x,y);
     }else{
         ctx.lineTo(x,y);
         ctx.stroke();
-        console.log("create line",x,y);
+        // console.log("create line",x,y);
     }
 }
 
@@ -36,10 +45,44 @@ const onMouseUp = (event) => {
 const onMouseLeave = (event) => {
     setPainting(false);
 }
+const handleCanvasClilck = (event) => {
+    if (filling) {
+        ctx.fillRect(0,0,canvas.width, canvas.height);
+    }
+}
+
+const handleModeChange = (event) => {
+    if (event.target.value == "line") {
+        filling = false;
+    } else if (event.target.value == "fill") {
+        filling = true;
+    }
+}
+
+const brushSizeChange = (event) => {
+    ctx.lineWidth = event.target.value;
+}
+
+const handleClearClick = (event) => {
+    ctx.clearRect(0,0,canvas.width, canvas.height);
+}
+
+const handleSaveClick = (event) => {
+    const image = canvas.toDataURL("");
+    const link = document.createElement("a");
+    link.href=image;
+    link.download = "sample";
+    link.click();
+}
 
 const changeColor = (event) => {
     console.log(event.target.style.backgroundColor);
     ctx.strokeStyle=event.target.style.backgroundColor;
+    ctx.fillStyle=event.target.style.backgroundColor;
+}
+
+const handleCM = (event) => {
+    event.preventDefault();
 }
 
 const init = () =>{
@@ -48,12 +91,36 @@ const init = () =>{
         canvas.addEventListener("mousedown", onMouseDown);
         canvas.addEventListener("mouseup", onMouseUp);
         canvas.addEventListener("mouseleave", onMouseLeave);
+        
+        canvas.addEventListener("click", handleCanvasClilck)
+        
+        // canvas 우클릭 드롭다운 메뉴 방지
+        canvas.addEventListener("contextmenu", handleCM)
 
-        ctx.strokeStyle=DEFAULT_STROKE_STYLE;
+        ctx.strokeStyle=DEFAULT_STYLE;
+        ctx.fillStyle=DEFAULT_STYLE;
         ctx.lineWidth=DEFAULT_LINE_WIDTH;
     }
+    
+    // mode
+    [...modeRadio].map((m)=>{
+        m.addEventListener("input", handleModeChange);
+    })
 
-    // console.log(colors)
+    if (brushSize) {
+        // brush size event
+        brushSize.addEventListener("input", brushSizeChange);
+    }
+
+    if (clearBtn) {
+        clearBtn.addEventListener("click", handleClearClick)
+    }
+
+    if (saveBtn) {
+        saveBtn.addEventListener("click", handleSaveClick)
+    }
+
+    // color event
     [...colors].map((c)=>{
         c.addEventListener("click", changeColor)
     })
